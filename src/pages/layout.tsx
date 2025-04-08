40f6070bab015e446dd307700cc411e41308e12e
tuyangjs <ihanlong@qq.com>
2025-04-08 09:56:25 +0800

    处理右键菜单

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -20,6 +20,20 @@ const AppInit = {
   logo: <img className='logo' src={logoimg}></img>
 }
 let times: NodeJS.Timeout
+document.addEventListener('keydown', function (e) {
+  if ((e.key === 'F5') || (e.ctrlKey && e.key === 'r')) {
+          e.preventDefault(); // 禁止刷新
+  }
+});
+        document.addEventListener('contextmenu', function (e) {
+  const target = e.target;
+        // @ts-ignore 如果目标是 input 元素且没有被禁用，则允许右键菜单
+        if (target.tagName.toLowerCase() === 'input' && !target.disabled) {
+    return;
+  }
+        // 其它情况（非 input 或 input 处于禁用状态）都阻止右键菜单
+        e.preventDefault();
+});
 const App: React.FC = () => {
   const { antdToken, themeConfig } = useTheme()
   const { colorBgContainer, colorBorder } = antdToken
